8c8eda941d4d7d57f8b005bf586237ad10c2a8f3
LucasSteinach <steinach@mail.ru>
2022-07-08 03:08:27 +0300

    #17

--- client_api.py
+++ client_api.py
@@ -1,25 +1,23 @@
-from flask import Flask
-from flask_restful import Api, Resource
-
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
 app = Flask(__name__)
 api = Api(app)
 
+# headers_args = reqparse.RequestParser()
+# headers_args.add_argument('client_id', type=str, help='Input id for Ozon API')
+# headers_args.add_argument('api_key', type=str, help='Input api_key for Ozon API')
+
 
 class MarkActionsAPI(Resource):
-    def get(self):
-        return {'data': 'get'}
 
-    def post(self):
-        return {'data': 'post'}
+    def __init__(self, connection):
+        self.conn = connection
 
-    def delete(self):
-        return {'data': 'delete'}
+    def get(self, client_id):
+        cursor = self.conn.cursor().execute("SELECT * FROM mark_actions WHERE client_id_api = '" + client_id +"'")
+        return cursor
 
-    def put(self):
-        return {'data': 'put'}
-
-
-api.add_resource(MarkActionsAPI, '/mark_actions_api')
+api.add_resource(MarkActionsAPI, '/mark_actions_api/<string:client_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- req_tests.py
+++ req_tests.py
@@ -2,11 +2,11 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-resp = requests.get(BASE + 'mark_actions_api')
+resp = requests.get(BASE + 'mark_actions_api/sdfljk/vaao')
 print(resp.json())
-resp = requests.post(BASE + 'mark_actions_api')
+resp = requests.post(BASE + 'mark_actions_api/sdfljk/vaao')
 print(resp.json())
-resp = requests.delete(BASE + 'mark_actions_api')
+resp = requests.delete(BASE + 'mark_actions_api/sdfljk/vaao')
 print(resp.json())
-resp = requests.put(BASE + 'mark_actions_api')
+resp = requests.put(BASE + 'mark_actions_api/sdfljk/vaao')
 print(resp.json())
