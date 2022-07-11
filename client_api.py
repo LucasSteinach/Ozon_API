72e3e7f7d4e7992b0c4fc0342b9aa1cae83a3159
LucasSteinach <steinach@mail.ru>
2022-07-11 14:53:52 +0300

    #19

--- client_api.py
+++ client_api.py
@@ -1,28 +1,23 @@
 from flask import Flask, request
 from pprint import pprint
+from ozon_api_connector import sql_connection, sql_my_auth_data
+import psycopg2
 from flask_restful import Api, Resource, reqparse
+
 app = Flask(__name__)
 api = Api(app)
 
-# headers_args = reqparse.RequestParser()
-# headers_args.add_argument('client_id', type=str, help='Input id for Ozon API')
-# headers_args.add_argument('api_key', type=str, help='Input api_key for Ozon API')
-
-
 class MarkActionsAPI(Resource):
 
-    def __init__(self, connection):
-        self.conn = connection
-
-    def get(self, client_id):
-        api.add_resource(MarkActionsAPI, '/mark_actions_api/<string:client_id>')
-        cursor = self.conn.cursor().execute("SELECT * FROM mark_actions WHERE client_id_api = '" + client_id +"'")
-        print("SELECT * FROM mark_actions WHERE client_id_api = '" + client_id +"'")
-        # records = cursor.fetchall()
-        # pprint(records)
-        return cursor
+    def get(self, client_id_api):
+        connect = sql_connection(*sql_my_auth_data)
+        with connect.cursor() as pointer:
+            pointer.execute("SELECT * FROM mark_actions WHERE client_id_api = '" + client_id_api + "'")
+            records = pointer.fetchall()
+        return records
 
 
+api.add_resource(MarkActionsAPI,  '/mark_actions_api/<string:client_id_api>')
 
 
 if __name__ == '__main__':
--- req_tests.py
+++ req_tests.py
@@ -3,4 +3,4 @@ import requests
 BASE = "http://127.0.0.1:5000/"
 
 resp = requests.get(BASE + 'mark_actions_api/2663')
-print(resp.json())
+print(resp.status_code)
