6739f30bb4b6b149c394a54e1aa0880387160a31
LucasSteinach <steinach@mail.ru>
2022-07-13 16:05:41 +0300

    #20

--- client_api.py
+++ client_api.py
@@ -7,17 +7,23 @@ from flask_restful import Api, Resource, reqparse
 app = Flask(__name__)
 api = Api(app)
 
+get_args = reqparse.RequestParser()
+get_args.add_argument(name='client_id_api', type=str, help='correct client_id_api is required', required=True)
+get_args.add_argument(name='api_key', type=str, help='correct api_key is required', required=True)
+
+
 class MarkActionsAPI(Resource):
 
-    def get(self, client_id_api):
+    def get(self):
+        req_data = get_args.parse_args()
         connect = sql_connection(*sql_my_auth_data)
         with connect.cursor() as pointer:
-            pointer.execute("SELECT * FROM mark_actions WHERE client_id_api = '" + client_id_api + "'")
+            pointer.execute(f"SELECT * FROM mark_actions WHERE client_id_api = '{req_data['client_id_api']}'")
             records = pointer.fetchall()
-        return records
+        return {req_data['client_id_api']: records}
 
 
-api.add_resource(MarkActionsAPI,  '/mark_actions_api/<string:client_id_api>')
+api.add_resource(MarkActionsAPI,  '/mark_actions_api')
 
 
 if __name__ == '__main__':
--- req_tests.py
+++ req_tests.py
@@ -2,5 +2,9 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-resp = requests.get(BASE + 'mark_actions_api/2663')
-print(resp.status_code)
+resp = requests.get(BASE + 'mark_actions_api',
+                    data={'client_id_api': '2663',
+                          'api_key': '2342'
+                          }
+                    )
+print(resp.text)
